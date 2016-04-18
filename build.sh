51ee6318ebc806c8e8912ded87f6c3de24f070c0
Robin Suter <suter.robin@gmail.com>
2016-04-18 21:38:24 +0200

    travis: build manually

--- build.sh
+++ build.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-#Install Dependecies
-apt-get update 
-apt-get install -y --no-install-recommends mkdocs
-pip install python-markdown-math
+#git config
+git config user.name "Travis CI"
+git config user.email "r1suter@hsr.ch"
 
-#Build with mkdocs
-mkdocs gh-deploy --clean
+#Build with mkdocs into ./site
+mkdocs build --clean
+
+#Copy site away
+cp -R site/ ../
+
+# Git HEAD is detached - checkout gh-pages
+git fetch origin
+git checkout -b gh-pages origin/gh-pages
+
+#sync site
+rsync -av ../site/ .
+
+#commit
+git add .
+git commit -m "Deploy from Travis CI"
+git push
