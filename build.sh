d439534dcaac084e944bb110fe4cb5142561f798
Robin Suter <suter.robin@gmail.com>
2016-04-18 22:23:39 +0200

    adjusted build script

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-#git config
-git config user.name "Travis CI"
-git config user.email "r1suter@hsr.ch"
-
 #Build with mkdocs into ./site
 mkdocs build --clean
 
@@ -17,6 +13,8 @@ rsync -av site/ gh-pages/
 
 #commit
 cd gh-pages
+git config user.name "Travis CI"
+git config user.email "r1suter@hsr.ch"
 git add .
 git commit -m "Deploy from Travis CI"
 git push
